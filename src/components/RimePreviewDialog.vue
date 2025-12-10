83ca0932ab12dc4533bcce35cbd25cc2c3eb0264
dragonish <no.web.developer@outlook.com>
2025-12-10 11:41:38 +0800

    revert: "tweak(preview): 优化预览弹窗中的复制行为"
    This reverts commit 9728988474694de209ca3ef89c5429034062f58d.

--- src/components/RimePreviewDialog.vue
+++ src/components/RimePreviewDialog.vue
@@ -8,7 +8,7 @@
         <p v-for="(item, index) in content.split('\n')" :key="index" class="rime-preview-dialog-content">{{ item }}</p>
       </q-card-section>
       <q-card-actions align="right" class="text-primary">
-        <q-btn v-close-popup flat :label="t('button.copy')" @click="emit('copy')"></q-btn>
+        <q-btn v-close-popup flat :label="t('button.copy')" @click="onCopy"></q-btn>
         <q-btn v-close-popup flat :label="t('button.close')"></q-btn>
       </q-card-actions>
     </q-card>
@@ -18,7 +18,9 @@
 <script setup lang="ts">
 import { ref, watch } from 'vue';
 import { useI18n } from 'vue-i18n';
+import { useQuasar } from 'quasar';
 import { useColorStore } from '@/stores/color';
+import { copy } from '@/common/copy';
 import { type MessageSchema } from '@/locales/schema';
 
 const { t } = useI18n<{
@@ -27,15 +29,12 @@ const { t } = useI18n<{
   useScope: 'global',
 });
 
+const $q = useQuasar();
 const colorStore = useColorStore();
 
 const visibility = defineModel<boolean>('visibility', {
   required: true,
 });
-
-const emit = defineEmits<{
-  copy: [];
-}>();
 
 const content = ref('');
 
@@ -47,6 +46,14 @@ watch(
     }
   }
 );
+
+function onCopy() {
+  copy(content.value);
+  $q.notify({
+    message: t('message.copy'),
+    color: 'primary',
+  });
+}
 </script>
 
 <style scoped>
